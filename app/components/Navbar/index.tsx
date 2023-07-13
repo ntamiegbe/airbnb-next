6bbb8fbdf026bf1d9b003bd29570ee6aa5d3efa0
Ntami Egbe <ntamiegbe00@gmail.com>
2023-07-13 04:53:39 +0100

    feat: created search component

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -2,14 +2,16 @@
 
 import Image from "next/image";
 import Container from "../Container"
+import Search from "../Search";
 
 const Navbar = () => {
     return (
         <div className="w-full fixed z-10 shadow-sm bg-white">
-            <div className="py-4">
+            <div className="py-5 border-b">
                 <Container>
-                    <div className="flex items-center justify-between gap-3">
+                    <div className="flex items-center justify-between gap-3 md:gap-0">
                         <Image alt="Logo" className="hidden md:block cursor-pointer" src="/images/logo.png" height="100" width="100" />
+                        <Search />
                     </div>
                 </Container>
             </div>
